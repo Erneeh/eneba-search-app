3326853608b74756c5c0234e8c5f7c4264fc616c
erne.undze@gmail.com <erne.undze@gmail.com>
2026-01-03 12:20:02 +0200

    final fix

--- backend/db.js
+++ backend/db.js
@@ -3,22 +3,20 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./games.db");
 
 db.serialize(() => {
-     
+     db.run(`
+    CREATE TABLE games (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  price REAL NOT NULL,
+  discount INTEGER DEFAULT 0,
+  platform TEXT NOT NULL,
+  region TEXT NOT NULL,
+  image_url TEXT NOT NULL,
+  likes INTEGER DEFAULT 0
+    );
+ `);
+
 });
 
 module.exports = db;
-
-// db.run(`
-//     CREATE TABLE games (
-//   id INTEGER PRIMARY KEY AUTOINCREMENT,
-//   title TEXT NOT NULL,
-//   price REAL NOT NULL,
-//   discount INTEGER DEFAULT 0,
-//   platform TEXT NOT NULL,
-//   region TEXT NOT NULL,
-//   image_url TEXT NOT NULL,
-//   likes INTEGER DEFAULT 0
-//     );
-//  `);
-
 
